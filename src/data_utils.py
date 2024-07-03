1da72bd8dd96c3d26dc8a8fbbb22420e42c2bd33
Osman Ali <Osman.HussienFadlAli@uk.qbe.com>
2024-07-03 16:20:17 +0000

    YOLO still pending

--- src/data_utils.py
+++ src/data_utils.py
@@ -3,7 +3,33 @@ import os
 import rasterio
 from matplotlib import pyplot as plt
 
-def download_images(bucket_name, prefix, local_dir):
+def download_images_and_labels(bucket_name, prefix, local_dir):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    
+    # Verify bucket existence
+    if not bucket.exists():
+        print(f"Bucket {bucket_name} does not exist.")
+        return
+    
+    blobs = bucket.list_blobs(prefix=prefix)
+    found_any_blob = False
+    
+    for blob in blobs:
+        found_any_blob = True
+        print(f"Found blob: {blob.name}")  # Debugging 
+        if any(blob.name.endswith(ext) for ext in ['.jpeg', '.jpg', '.json', '.png', '.csv', '.txt']):
+            
+            # Create local directory structure if it doesn't exist
+            local_path = os.path.join(local_dir, os.path.relpath(blob.name, prefix))
+            os.makedirs(os.path.dirname(local_path), exist_ok=True)
+            blob.download_to_filename(local_path)
+            print(f"Downloaded {blob.name} to {local_path}")
+    
+    if not found_any_blob:
+        print("No blobs found with the given prefix.")
+            
+def download_tiff_images(bucket_name, prefix, local_dir):
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     
